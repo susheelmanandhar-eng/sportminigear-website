6d77733c57ced3ee5e44c317d1ff2fce68c22132
susheelmanandhar-eng <susheel.manandhar@gmail.com>
2026-09-15 13:39:36 +0545

    updated product

--- js/products-data.js
+++ js/products-data.js
@@ -439,6 +439,24 @@ colors: [
     description: 
 "Product Name - This mini pocket cycling bike repair multi-tool is made of durable ABS and metal materials, featuring a compact size of 9*2.5*4.5CM. Made with high-quality stainless steel tools and a sturdy ABS body, it combines strength, portability, and convenience. The tool includes multiple hex keys, screwdrivers, spoke wrenches, and other essential functions needed for roadside fixes. Its foldable design makes it easy to carry in your pocket, saddlebag, or toolkit, ensuring you’re always prepared for unexpected repairs during rides. Perfect for cyclists, mountain bikers, and daily commuters who value reliability and efficiency.  Product Name - Bike Repair Tool Material - ABS + Metal Size - 9*2.5*4.5CM.  Weight - 300 gm Feature - High quality, heavy stainless steel,  Usage - Promotion/Business gift, dailiy usage, outdoor/indoor tools, bottle opener, screwdriver, hex wrench.",
   },
+{
+    id: "cy-004",
+    name: "Sports Armband Multifunctional Pockets Exercise Workout Running Waterproof Arm Bag with Earphone Hole",
+    category: "Cycling",
+    price: 375,
+    oldPrice: 450,
+    images: [
+      "images/products/cy-004-1.jpg",
+      "images/products/cy-004-2.jpg",
+      "images/products/cy-004-3.jpg"
+  ],
+
+    badge: "New",
+    stock: 5,
+    description: 
+"This armband is generally useful for outdoor related sports activities to keep your important accessories safe and easy access. Product Name - Sports Armband for Multifunctional Usage
+Feature - Waterproof Durable Material - Neoprene Capacity - 10L Use - Camping and Hiking Package - One Poly Bag Colors - Blue/Green/Orange/Pink",
+  },
 
 
    {
