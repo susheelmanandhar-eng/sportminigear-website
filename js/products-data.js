0aa14571141e82b03097fa8e21456452598b00de
susheelmanandhar-eng <susheel.manandhar@gmail.com>
2026-09-22 16:33:00 +0545

    updated

--- js/products-data.js
+++ js/products-data.js
@@ -163,7 +163,7 @@ const PRODUCTS = [
     id: "fb-006",
     name: "Lightweight Mylar Emergency Thermal Bivy Camping Outdoor Sleeping Bag with Drawstring",
     category: "Travel Gear",
-    price: 675,
+    price: 10,
     images: [
       "images/products/fb-006-1.jpg",
       "images/products/fb-006-2.jpg",
